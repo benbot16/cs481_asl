be717b6a8e382e6a930171bc4d0bc21c8c498742
Benjamin <9423435+benbot16@users.noreply.github.com>
2025-05-19 15:04:38 -0700

    Inits some node stuff

--- js/firebase.js
+++ js/firebase.js
@@ -1,3 +1,36 @@
 use strict;
 
+import { initializeApp } from 'firebase/app';
+import {
+  GoogleAuthProvider,
+  User,
+  connectAuthEmulator,
+  getAuth,
+  onAuthStateChanged,
+  signInWithPopup,
+  signOut,
+} from 'firebase/auth';
+import {
+  DatabaseReference,
+  Query,
+  child,
+  connectDatabaseEmulator,
+  getDatabase,
+  limitToLast,
+  off,
+  onChildAdded,
+  onChildChanged,
+  onChildRemoved,
+  onValue,
+  orderByChild,
+  push,
+  query,
+  ref,
+  runTransaction,
+  set,
+  update,
+} from 'firebase/database';
+import { firebaseConfig } from './config';
+
+const app = initializeApp(firebaseConfig)
 
