bf8a884546d1d5ef5883ca0be98130d997130a0b
JamWild96 <100748711+JamWild96@users.noreply.github.com>
2025-05-27 11:39:18 -0700

    added camera permission/sign history display under

--- asl/src/app/pages/sign-page/sign-page.component.ts
+++ asl/src/app/pages/sign-page/sign-page.component.ts
@@ -1,5 +1,5 @@
 import { AsyncPipe } from '@angular/common';
-import { Component, inject } from '@angular/core';
+import { Component, ElementRef, ViewChild, inject, OnInit } from '@angular/core';
 import { DocumentData } from '@angular/fire/firestore';
 import { FormsModule } from '@angular/forms';
 import { Observable } from 'rxjs';
@@ -12,8 +12,27 @@ import { UserService } from 'src/app/services/user.service';
   standalone: true,
   imports: [AsyncPipe, FormsModule]
 })
-export class SignPageComponent {
+export class SignPageComponent implements OnInit {
   userService = inject(UserService);
   user$ = this.userService.user$;
   text = '';
+  signedLetters = ''; // Store the history of signed letters
+
+  @ViewChild('videoElement') videoElement!: ElementRef<HTMLVideoElement>;
+
+  async ngOnInit() {
+    try {
+      // Request camera permissions when component initializes
+      const stream = await navigator.mediaDevices.getUserMedia({
+        video: true,
+        audio: false
+      });
+
+      // Once we have permissions, set up the video stream
+      this.videoElement.nativeElement.srcObject = stream;
+    } catch (err) {
+      console.error('Error accessing camera:', err);
+      // You might want to show an error message to the user here
+    }
+  }
 }
